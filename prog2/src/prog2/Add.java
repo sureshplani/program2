83948b196fc5a72ebf66872829c975ba565136e7
sureshpalani <suresh16palani@mail.com>
2022-04-01 22:24:22 +0530

    Bitwise operator

---
+++ prog2/src/prog2/Add.java
@@ -0,0 +1,17 @@
+package prog2;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=23;
+		if((num&1)==0) {
+			System.out.println("even number:"+num);
+		}
+		else { 
+				System.out.println("add number:"+num);
+		}
+
+	}
+
+}
